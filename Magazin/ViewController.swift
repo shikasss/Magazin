e53a8960517a24baa2e7ea524450ea1dbb3e0197
Sergey Shinkarenko <sergey.shinkarenko@onde.app>
2021-04-30 21:19:18 +0300

    Initial Commit

---
+++ Magazin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magazin
+//
+//  Created by Sergey Shinkarenko on 30.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
